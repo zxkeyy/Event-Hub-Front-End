04a3174aa25c236ca722bcec73b18e837f02a7ec
zxkeyy <z_selmani@estin.dz>
2023-08-04 16:58:50 +0100

    Added a search to nav bar (not-func)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from "../assets/logo.jpeg"
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchField from './SearchField'
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between">
+    <HStack bgColor="purple.800" padding={2}>
         <Image src={logo} boxSize='60px'/>
+        <SearchField/>
         <ColorModeSwitch />
     </HStack>
   )
---
+++ src/components/SearchField.tsx
@@ -0,0 +1,13 @@
+import { ButtonGroup, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { BsSearch} from "react-icons/bs"
+
+const SearchField = () => {
+  return (
+    <InputGroup>
+        <InputLeftElement children={<BsSearch/>}/>
+      <Input borderRadius={20} placeholder="search" variant="filled"/>
+    </InputGroup>
+  );
+};
+
+export default SearchField;
