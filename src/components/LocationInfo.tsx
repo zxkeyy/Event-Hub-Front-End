4a55f029b4c542ac59238fcadd2d93d1e38b5c93
zxkeyy <z_selmani@estin.dz>
2023-08-10 17:13:17 +0100

    Refractored OSMap into component

--- src/components/LocationInfo.tsx
+++ src/components/LocationInfo.tsx
@@ -2,6 +2,7 @@ import { Box, HStack, Text } from "@chakra-ui/react";
 import { BsGeoAlt } from "react-icons/bs";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import useMapLookup from "../hookers/useMapLookup";
+import OSMap from "./OSMap";
 
 interface Props {
   locationId: string;
@@ -11,7 +12,7 @@ interface Props {
 const LocationInfo = ({ locationId, locationName }: Props) => {
   const { data, error } = useMapLookup(locationId);
 
-  if(error) throw(error);
+  if (error) throw error;
 
   return (
     <Box>
@@ -19,22 +20,7 @@ const LocationInfo = ({ locationId, locationName }: Props) => {
         <BsGeoAlt size={40} />
         <Text fontWeight="semibold">{locationName}</Text>
       </HStack>
-      {data && data[0] && (
-        <MapContainer
-          center={[parseFloat(data[0].lat), parseFloat(data[0].lon)]}
-          zoom={15}
-          scrollWheelZoom={false}
-          style={{ height: "300px", width: "100%" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[parseFloat(data[0].lat), parseFloat(data[0].lon)]}>
-            <Popup>{data[0].name}</Popup>
-          </Marker>
-        </MapContainer>
-      )}
+      {data && data[0] && <OSMap locationData={data[0]} />}
     </Box>
   );
 };
---
+++ src/components/OSMap.tsx
@@ -0,0 +1,29 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { LocationData } from "../hookers/useMapLookup";
+
+interface Props {
+  locationData: LocationData;
+}
+
+const OSMap = ({ locationData }: Props) => {
+  return (
+    <MapContainer
+      center={[parseFloat(locationData.lat), parseFloat(locationData.lon)]}
+      zoom={15}
+      scrollWheelZoom={false}
+      style={{ height: "300px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker
+        position={[parseFloat(locationData.lat), parseFloat(locationData.lon)]}
+      >
+        <Popup>{locationData.name}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default OSMap;
