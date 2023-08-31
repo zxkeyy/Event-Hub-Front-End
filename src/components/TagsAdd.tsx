c72fd40f93107f433a79e6f6792a6f9b9868990c
zxkeyy <z_selmani@estin.dz>
2023-08-31 17:04:23 +0100

    idk

--- src/components/TagsAdd.tsx
+++ src/components/TagsAdd.tsx
@@ -14,7 +14,7 @@ import {
 } from "@chakra-ui/react";
 import useTags, { postTag } from "../hookers/useTags";
 import { useRef, useState } from "react";
-import { BsPlus, BsPlusLg } from "react-icons/bs";
+import { BsPlusLg } from "react-icons/bs";
 
 interface Props {
   tags: number[];
--- src/pages/CreateEventPage.tsx
+++ src/pages/CreateEventPage.tsx
@@ -11,7 +11,6 @@ import {
   Link,
   Menu,
   MenuButton,
-  MenuDivider,
   MenuItem,
   MenuList,
   Select,
