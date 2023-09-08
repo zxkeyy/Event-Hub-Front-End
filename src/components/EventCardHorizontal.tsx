8009d92d2df89797875c28694eb9a5e728c95220
zxkeyy <z_selmani@estin.dz>
2023-09-08 16:16:46 +0100

    Event cards and stack

---
+++ src/components/EventCardHorizontal.tsx
@@ -0,0 +1,111 @@
+import {
+  useColorModeValue,
+  Card,
+  CardBody,
+  Grid,
+  GridItem,
+  Heading,
+  Button,
+  Image,
+  Text,
+  LinkBox,
+  LinkOverlay,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  IconButton,
+} from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+import parseDate from "../services/parse-date";
+import { Event } from "../hookers/useEvents";
+import fallBackImage from "../assets/image-not-found.png";
+import { BiEdit } from "react-icons/bi";
+
+interface Props {
+  event: Event;
+}
+
+const EventCardHorizontal = ({ event }: Props) => {
+  const bgColor = useColorModeValue("white", "purple.900");
+  const borderColor = useColorModeValue("gray.300", "gray.800");
+
+  return (
+    <LinkBox width="100%">
+      <Card
+        width="100%"
+        direction="row"
+        bgColor={bgColor}
+        border="1px"
+        borderRadius={10}
+        borderColor={borderColor}
+        overflow="hidden"
+        shadow="md"
+        _hover={{
+          transform: "scale(1.005)",
+          transition: "transform .15s ease-in",
+        }}
+      >
+        <Image
+          aspectRatio={1 / 1}
+          boxSize={40}
+          src={event.image}
+          fallbackSrc={fallBackImage}
+        />
+        <CardBody padding={3}>
+          <Grid
+            templateAreas={`"title primary" "title secondary"`}
+            templateColumns={"1fr 0.2fr"}
+            paddingTop={0}
+          >
+            <GridItem area="title" paddingY={5}>
+              <Text fontSize="sm">
+                {parseDate(event.start_date, event.end_date).toUpperCase()}
+              </Text>
+              <LinkOverlay href={"/events/" + event.slug}>
+                <Heading size="md" fontSize="md" noOfLines={2}>
+                  {event.name}
+                </Heading>
+              </LinkOverlay>
+              <Text fontSize="sm" color="gray.500" noOfLines={2}>
+                {event.location_name}
+              </Text>
+            </GridItem>
+            <GridItem area="primary" padding={2}>
+              <Button
+                width="full"
+                colorScheme="purple"
+                fontSize="sm"
+                bgColor="purple.400"
+                leftIcon={<BiEdit />}
+              >
+                edit
+              </Button>
+            </GridItem>
+            <GridItem area="secondary" padding={2}>
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  icon={<BsThreeDots />}
+                  width="full"
+                  colorScheme="purple"
+                  fontSize="sm"
+                  bgColor="purple.400"
+                ></MenuButton>
+                <Portal>
+                  <MenuList>
+                    <MenuItem>Share</MenuItem>
+                    <MenuItem>Delete Event</MenuItem>
+                  </MenuList>
+                </Portal>
+              </Menu>
+            </GridItem>
+          </Grid>
+        </CardBody>
+      </Card>
+    </LinkBox>
+  );
+};
+
+export default EventCardHorizontal;
--- src/pages/MyEventsPage.tsx
+++ src/pages/MyEventsPage.tsx
@@ -1,14 +1,65 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Divider, HStack, Heading, Stack } from "@chakra-ui/react";
+import EventCardHorizontal from "../components/EventCardHorizontal";
+import useEvents from "../hookers/useEvents";
+import { BiLoaderCircle } from "react-icons/bi";
 
 const MyEventsPage = () => {
+  const { data: events, isLoading } = useEvents();
+
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" padding={20}
-      style=
-      {{
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      padding={20}
+      style={{
         background:
           "linear-gradient(90deg, rgba(20,1,32,1) 0%, rgba(46,8,78,1) 81%, rgba(48,0,84,1) 100%)",
-      }}>
-      
+      }}
+    >
+      <Box width="50%">
+        <Box
+          border="1px"
+          borderRadius={15}
+          borderColor="whiteAlpha.200"
+          bgColor="purple.900"
+          padding={5}
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          marginBottom={3}
+        >
+          <Heading fontSize="2xl">Your Events</Heading>
+          <Divider marginY={2} />
+          <HStack
+            display="flex"
+            justifyContent="start"
+            width="100%"
+            height={10}
+          >
+            <Button height="100%">Upcoming</Button>
+            <Button height="100%">Past</Button>
+          </HStack>
+        </Box>
+        <Box
+          border="1px"
+          borderRadius={15}
+          borderColor="whiteAlpha.200"
+          bgColor="purple.900"
+          padding={5}
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Stack spacing={6}>
+            {isLoading && <BiLoaderCircle />}
+            {events?.results.map((event) => (
+              <EventCardHorizontal event={event} />
+            ))}
+          </Stack>
+        </Box>
+      </Box>
     </Box>
   );
 };
