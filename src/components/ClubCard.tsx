c4450cecf5038f10a9850884ee0898dc0340cb5b
zxkeyy <z_selmani@estin.dz>
2023-09-15 17:26:29 +0100

    Fixed linking to hosts

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -1,4 +1,12 @@
-import { Avatar, Box, Button, Divider, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Link,
+  LinkOverlay,
+  Text,
+} from "@chakra-ui/react";
 import useClub from "../hookers/useClub";
 
 interface Props {
@@ -9,31 +17,37 @@ const ClubCard = ({ clubId }: Props) => {
   const { data: club } = useClub(clubId);
 
   return (
-    <Box
-      border="1px"
-      borderRadius={15}
-      borderColor="whiteAlpha.200"
-      bgColor="purple.900"
-      padding={5}
-      marginTop={2}
-      display="flex"
-      alignItems="center"
-      flexDirection="column"
-      boxShadow="2xl"
-    >
-      <Avatar size="2xl" src={club?.image} />
-      <Text fontWeight="bold" padding={2}>{club?.name}</Text>
-      <Divider />
-      <Button
-        width="full"
-        colorScheme="purple"
-        fontSize="sm"
-        bgColor="purple.400"
+    <LinkOverlay>
+      <Box
+        border="1px"
+        borderRadius={15}
+        borderColor="whiteAlpha.200"
+        bgColor="purple.900"
+        padding={5}
         marginTop={2}
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        boxShadow="2xl"
       >
-        See more
-      </Button>
-    </Box>
+        <Avatar size="2xl" src={club?.image} />
+        <Text fontWeight="bold" padding={2}>
+          {club?.name}
+        </Text>
+        <Divider />
+        <Link href={"/hosts/" + club?.id} width="full">
+          <Button
+            width="full"
+            colorScheme="purple"
+            fontSize="sm"
+            bgColor="purple.400"
+            marginTop={2}
+          >
+            See more
+          </Button>
+        </Link>
+      </Box>
+    </LinkOverlay>
   );
 };
 
